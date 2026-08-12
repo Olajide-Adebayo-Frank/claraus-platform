b817025b4e68226bcad1c6922ded1bbab3f2f389
Olajide Adebayo Frank <olajideadebayo93@gmail.com>
2026-08-12 07:52:05 -0700

    Add sanitized application loader example

---
+++ examples/application/Loader.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Claraus\Example;
+
+final class Loader
+{
+    public static function load(): void
+    {
+        // Initialize Claraus application modules.
+    }
+}
